83892d17f94b9093eb59fbbe5185bc87bca4420b
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-05-18 16:15:11 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -1,104 +1,20 @@
 #include "shell.h"
 
-int token_len(char *str, char *delim);
-int count_tokens(char *str, char *delim);
-char **_strtok(char *line, char *delim);
-
 /**
- * token_len - Locates the delimiter index marking the end
- *             of the first token contained within a string.
- * @str: The string to be searched.
- * @delim: The delimiter character.
- *
- * Return: The delimiter index marking the end of
- *         the intitial token pointed to be str.
+ * main - Entry point for shell, handles args to shell
+ * @ac: Arg count
+ * @av: Arr of args
+ * @env: Environment
+ * Return: 0;
  */
-int token_len(char *str, char *delim)
+int main(int ac, char **av, char **env)
 {
-	int index = 0, len = 0;
-
-	while (*(str + index) && *(str + index) != *delim)
-	{
-		len++;
-		index++;
-	}
-
-	return (len);
-}
-
-/**
- * count_tokens - Counts the number of delimited
- *                words contained within a string.
- * @str: The string to be searched.
- * @delim: The delimiter character.
- *
- * Return: The number of words contained within str.
- */
-int count_tokens(char *str, char *delim)
-{
-	int index, tokens = 0, len = 0;
-
-	for (index = 0; *(str + index); index++)
-		len++;
-
-	for (index = 0; index < len; index++)
-	{
-		if (*(str + index) != *delim)
-		{
-			tokens++;
-			index += token_len(str + index, delim);
-		}
-	}
-
-	return (tokens);
-}
-
-/**
- * _strtok - Tokenizes a string.
- * @line: The string.
- * @delim: The delimiter character to tokenize the string by.
- *
- * Return: A pointer to an array containing the tokenized words.
- */
-char **_strtok(char *line, char *delim)
-{
-	char **ptr;
-	int index = 0, tokens, t, letters, l;
-
-	tokens = count_tokens(line, delim);
-	if (tokens == 0)
-		return (NULL);
-
-	ptr = malloc(sizeof(char *) * (tokens + 2));
-	if (!ptr)
-		return (NULL);
-
-	for (t = 0; t < tokens; t++)
-	{
-		while (line[index] == *delim)
-			index++;
-
-		letters = token_len(line + index, delim);
-
-		ptr[t] = malloc(sizeof(char) * (letters + 1));
-		if (!ptr[t])
-		{
-			for (index -= 1; index >= 0; index--)
-				free(ptr[index]);
-			free(ptr);
-			return (NULL);
-		}
-
-		for (l = 0; l < letters; l++)
-		{
-			ptr[t][l] = line[index];
-			index++;
-		}
-
-		ptr[t][l] = '\0';
-	}
-	ptr[t] = NULL;
-	ptr[t + 1] = NULL;
-
-	return (ptr);
+	if (!ac)
+		(void)ac;
+	if (!av)
+		(void)av;
+	if (!env)
+		(void)env;
+	shell(ac, av, env);
+	return (0);
 }
