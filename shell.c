3268ae2aef148ffd74a477b60a013f17e2d50339
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-05-18 17:02:25 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -1,20 +1,47 @@
 #include "shell.h"
-
 /**
- * main - Entry point for shell, handles args to shell
+ * shell - Infinite loop that runs shell
  * @ac: Arg count
- * @av: Arr of args
+ * @av: args passed to shell at beginning of prog
  * @env: Environment
- * Return: 0;
+ * Return: Void
  */
-int main(int ac, char **av, char **env)
+void shell(int ac, char **av, char **env)
 {
+	char *line;
+	char **args;
+	int status = 1;
+	char *tmp = NULL;
+	char *er;
+	char *filename;
+	int flow;
+
+	er = "Error";
+	do {
+		prompt();
+		line = _getline();
+		args = split_line(line);
+		flow = bridge(args[0], args);
+		if (flow == 2)
+		{
+			filename = args[0];
+			args[0] = find_path(args[0], tmp, er);
+			if (args[0] == er)
+			{
+				args[0] = search_cwd(filename, er);
+				if (args[0] == filename)
+					write(1, er, 5);
+			}
+		}
+		if (args[0] != er)
+			status = execute_prog(args, line, env, flow);
+		free(line);
+		free(args);
+	} while (status);
 	if (!ac)
 		(void)ac;
 	if (!av)
 		(void)av;
 	if (!env)
 		(void)env;
-	shell(ac, av, env);
-	return (0);
 }
